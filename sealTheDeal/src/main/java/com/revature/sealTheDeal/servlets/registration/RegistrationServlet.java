9a18b39d6fa31e2c9f2eac60b96eddde54d0d66b
Brandon-Clark-SC <brandon.clark.sc@gmail.com>
2022-02-15 15:48:10 -0800

    did a lot of playing around with html backgrounds, did some funny stuff, made it look cool, added registration buttons

--- sealTheDeal/src/main/java/com/revature/sealTheDeal/servlets/registration/RegistrationServlet.java
+++ sealTheDeal/src/main/java/com/revature/sealTheDeal/servlets/registration/RegistrationServlet.java
@@ -1,6 +1,7 @@
 package com.revature.sealTheDeal.servlets.registration;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,7 +14,31 @@ public class RegistrationServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().write("<h1>TestServlet works</h1>");
+		
+		resp.setContentType("text/html");
+	    PrintWriter out = resp.getWriter();
+	    out.println("<style>"
+	    		+ "body {"
+	    		+ "background-image: url('https://ih1.redbubble.net/image.291419102.1980/st,small,507x507-pad,600x600,f8f8f8.u3.jpg');"
+	    		+ "background-repeat: no-repeat;"
+	    		+ "background-attachment: fixed;"
+	    		+ "background-size: contain;"
+	    		+ "background-position: center;"
+	    		+ "background-color: black;"
+	    		+ "}"
+	    		+ "</style>");
+	    out.println("<form action=\"http://localhost:8080/sealTheDeal/registration/weddingUser/\">"
+	    		+ "<input type=\"submit\" value=\"Register Wedding Party\">"
+	    		+ "</form>");
+		out.println("<form action=\"http://localhost:8080/sealTheDeal/registration/guest/\">"
+	    		+ "<input type=\"submit\" value=\"Register Guest\">"
+	    		+ "</form>");
+		out.println("<form action=\"http://localhost:8080/sealTheDeal/registration/employee/\">"
+	    		+ "<input type=\"submit\" value=\"Register Employee\">"
+	    		+ "</form>");
+		out.println("<form action=\"http://localhost:8080/sealTheDeal/\">"
+	    		+ "<input type=\"submit\" value=\"Return\">"
+	    		+ "</form>");
 	}
 	
 }
