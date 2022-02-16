441e43769047b6b6771e94c7ed39b8d45c85a5b6
andrewdsnyder <andrew3snyder@gmail.com>
2022-02-15 17:24:24 -0800

    added boilerplate/start to DAOs, models, services. Made a connectionFactory, added to ContextLoaderListener.

--- sealTheDeal/src/main/java/com/revature/sealTheDeal/models/Guest.java
+++ sealTheDeal/src/main/java/com/revature/sealTheDeal/models/Guest.java
@@ -2,4 +2,27 @@ package com.revature.sealTheDeal.models;
 
 public class Guest extends User {
 
+	private int guestId;
+	private User user;
+	private String  weddingPartyName;
+	
+	
+	public int getGuestId() {
+		return guestId;
+	}
+	public void setGuestId(int guestId) {
+		this.guestId = guestId;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getWeddingPartyName() {
+		return weddingPartyName;
+	}
+	public void setWeddingPartyName(String weddingPartyName) {
+		this.weddingPartyName = weddingPartyName;
+	}
 }
