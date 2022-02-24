8317366a354574eb2dbb8e69c5217080fda8df58
andrewdsnyder <andrew3snyder@gmail.com>
2022-02-24 14:02:25 -0800

    updated contextLoaderListener with ViewWeddingDetails, finished ViewWeddingDetailsServlet

--- sealTheDeal/src/main/java/com/revature/sealTheDeal/servlets/guest/ViewWeddingDetailsServlet.java
+++ sealTheDeal/src/main/java/com/revature/sealTheDeal/servlets/guest/ViewWeddingDetailsServlet.java
@@ -15,36 +15,75 @@ import com.revature.sealTheDeal.services.GuestServices;
 import com.revature.sealTheDeal.services.WeddingUserServices;
 
 public class ViewWeddingDetailsServlet extends HttpServlet {
-	
-	
+
 	String message = null;
 	Guest currentGuest;
 	GuestServices guestServices;
 	ObjectMapper mapper;
 	WeddingUserServices weddingUserServices;
 	WeddingUser weddingUser;
-	
+
+	public ViewWeddingDetailsServlet(GuestServices guestServices, WeddingUserServices weddingUserServices,
+			ObjectMapper mapper) {
+		this.guestServices = guestServices;
+		this.weddingUserServices = weddingUserServices;
+		this.mapper = mapper;
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		currentGuest = guestServices.getSessionGuest();
 		weddingUser = weddingUserServices.getByWeddingName(currentGuest.getWeddingPartyName());
+		String florist = "no florist currently booked";
+		String caterer = "no caterer currently booked";
+		String photographer = "no photographer currently booked";
+		String musician = "no caterer currently booked";
+		String venue = "no venue currently booked...WHERE IS THE WEDDING?!";
+
+		if (!(weddingUser.getBookedFlorist().equals(""))) {
+			florist = weddingUser.getBookedFlorist();
+		}
+		if (!(weddingUser.getBookedCaterer().equals(""))) {
+			caterer = weddingUser.getBookedCaterer();
+		}
+		if (!(weddingUser.getBookedPhotographer().equals(""))) {
+			photographer = weddingUser.getBookedPhotographer();
+		}
+		if (!(weddingUser.getBookedMusician().equals(""))) {
+			musician = weddingUser.getBookedMusician();
+		}
+		if (!(weddingUser.getBookedVenue().equals(""))) {
+			venue = weddingUser.getBookedVenue();
+		}
+
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
 		out.println("<style>" + "body {"
 				+ "background-image: url('https://ih1.redbubble.net/image.291419102.1980/st,small,507x507-pad,600x600,f8f8f8.u3.jpg');"
 				+ "background-repeat: no-repeat;" + "background-attachment: fixed;" + "background-size: contain;"
 				+ "background-position: center;" + "background-color: grey;" + "}" + "</style>");
-		
+
 		out.print("<h3>Hello, " + currentGuest.getFirstName() + "</h3>");
 		out.println("<h3> Details for the" + currentGuest.getWeddingPartyName() + " wedding</h3>");
-		if(currentGuest.getPlusOne().equals("")) {
+		out.println("<h3> The day of the wedding is:" + weddingUser.getDayOfWedding() + "</h3>");
+		out.println("<h3> The venue of the wedding is:" + venue + "</h3>");
+		out.println("<h3> The caterer for the wedding will be:" + caterer + "</h3>");
+		out.println("<h3> You have ordered" + currentGuest.getFoodType() + "for dinner</h3>");
+
+		if (currentGuest.getPlusOne().equals("")) {
 			out.println("<h3>You do not currently have a Plus One registered</h3>");
-		} else { out.println("<h3> Your Plus One," + currentGuest.getPlusOne(req.getParameter("plus_one_name")) + "has ordered" + );
+		} else {
+			out.println("<h3> Your Plus One," + currentGuest.getPlusOne() + "has ordered"
+					+ currentGuest.getPlusOneFoodType() + "</h3>");
 		}
+
+		out.println("<h3> The florist for the wedding will be:" + florist + "</h3>");
+		out.println("<h3> With music by:" + musician + "</h3>");
+		out.println("<h3> Professional photography by:" + photographer + "</h3>");
 		
-		}
-		out.println("<h3> You have ordered" + currentGuest.getFoodType() + "for dinner"</h3>);
-}
-	
-	
+		out.println("<form action=\"http://localhost:8080/sealTheDeal/guestHome/\">"
+				+ "<input type=\"submit\" value=\"Return\">" + "</form>");
+
+	}
+
 }
