29690d0c9c075659eb5e7b6513acb653b7c6132d
Brandon Clark <brandon.clark.sc@gmail.com>
2022-02-24 13:11:52 -0800

    working on meal plans, added a guest meal variable

--- sealTheDeal/src/main/java/com/revature/sealTheDeal/models/Guest.java
+++ sealTheDeal/src/main/java/com/revature/sealTheDeal/models/Guest.java
@@ -18,6 +18,9 @@ public class Guest extends User {
 	@Column(name = "plus_one_name")
 	private String plusOne;
 	
+	@Column(name = "plus_one_food_type")
+	private String plusOneFoodType;
+	
 	@Column(name = "is_attending")
 	private boolean isAttending;
 	
@@ -25,11 +28,12 @@ public class Guest extends User {
 		super();
 	}
 	
-	public Guest(String username, String firstName, String lastName, String pass, String user_email, int accountType, String weddingPartyName, String foodType, String plusOne, boolean isAttending) {
+	public Guest(String username, String firstName, String lastName, String pass, String user_email, int accountType, String weddingPartyName, String foodType, String plusOne, String plusOneFoodType, boolean isAttending) {
 		super(username, firstName, lastName, pass, user_email, accountType);
 		this.weddingPartyName = weddingPartyName;
 		this.foodType = foodType;
 		this.plusOne = plusOne;
+		this.plusOneFoodType = plusOneFoodType;
 		this.isAttending = isAttending;
 	}
 	
@@ -67,6 +71,14 @@ public class Guest extends User {
 
 	public void setAttendance(boolean isAttending) {
 		this.isAttending = isAttending;
+	}
+
+	public String getPlusOneFoodType() {
+		return plusOneFoodType;
+	}
+
+	public void setPlusOneFoodTypel(String plusOneFoodType) {
+		this.plusOneFoodType = plusOneFoodType;
 	}
 
 }
